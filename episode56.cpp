6354dce324e75286e2e7eb2bbb094106772a229e
Divyansh Mittal <mittaldivyansh2212@gmail.com>
2021-11-17 18:08:29 +0530

    17/11/21

---
+++ episode56.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // (a^b) % m = (a ^ (b % etf(m))) % m;
+    // ETF of m = m * sigma(1 - 1/pf);  pf ----> prime factors
+    // ETF of m when m is prime is m-1.
+
+    // arr[]= {1, 2, 3, 8, 9};
+    // To form a number using this array
+    // ans = 0;
+    // for(int i=0; i < arr.size(); i++)
+    // {
+    //    ans = (ans*10) + arr[i];
+    // }
+    // return ans;
+return 0;
+}
