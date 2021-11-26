76df7d320263dd1a8ce116f0074ad2fb5442fefd
Divyansh Mittal <mittaldivyansh2212@gmail.com>
2021-11-26 22:33:08 +0530

    26/11/21

--- test.cpp
+++ test.cpp
@@ -1,50 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int M = 1e9 + 7;
-long long binExp(long long a, long long b)
+int binExp(int a, int b)
 {
-   long long ans = 1;
+   int ans = 1;
    while (b > 0)
    {
       if (b & 1)
-         ans = (ans * a) % M;
-      a = (a * a) % M;
+         ans = (ans * 1LL * a) % M;
+      a = (a * 1LL * a) % M;
       b = (b >> 1);
    }
    return ans;
 }
 int main()
 {
-   int t;
-   cin >> t;
-   while (t--)
+   long long sum = 0;
+   int faculties;
+   cin >> faculties;
+   while (faculties--)
    {
-      int cnt;
-      cin >> cnt;
-      vector<long long> numbers(cnt);
-      vector<long long> powers(cnt);
-      vector<long long> gps(cnt);
-      for (int i = 0; i < cnt; i++)
-         cin >> numbers[i];
-
-      for (int i = 0; i < cnt; i++)
-         cin >> powers[i];
-
-      for (int i = 0; i < cnt; i++)
+      int courses;
+      cin >> courses;
+      sum = sum + binExp(2, courses) - 1;
+      sum = sum % M;
+      while (courses--)
       {
-         long long numera = (numbers[i] * (binExp(numbers[i], powers[i]) - 1)) % M;
-         long long deno = numbers[i] - 1;
-         long long ele = (numera * binExp(deno, M - 2)) % M;
-         gps[i] = ele;
+         int points;
+         cin >> points;
       }
-
-         long long ans = 1;
-         for (long long i = 0; i < gps.size(); i++)
-         {
-            ans = (ans * gps[i]) % M;
-         }
-         cout << ans << endl;
    }
-
+   cout << sum;
    return 0;
 }
