7f7206e086e52001eea788be58ec47239bdc141d
Divyansh Mittal <mittaldivyansh2212@gmail.com>
2021-12-07 17:26:57 +0530

    07/12/21

--- test.cpp
+++ test.cpp
@@ -1,65 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 2e5 + 10;
+const int N = 1e5 + 10;
 vector<int> g[N];
 int val[N];
-int curr_ans1 = 0;
-int curr_ans2 = 0;
+int cnt;
 
-void dfs1(int vtx, int par = 0)
+void dfs(int vtx, int par, long long target, long long curr_sum, int curr_cnt)
 {
+   if (par != 0)
+   {
+      curr_sum += val[vtx];
+      curr_cnt++;
+   }
+   if (curr_sum >= target)
+   {
+      cnt = min(cnt, curr_cnt);
+      return;
+   }
    for (auto child : g[vtx])
    {
       if (child == par)
          continue;
-
-      if (val[child] == 1)
-      {
-         if (val[par] == 0 && par != 0)
-         {
-            if (child < vtx)
-               curr_ans1++;
-            else if (child > vtx)
-               curr_ans2++;
-         }
-         continue;
-      }
-      dfs1(child, vtx);
-
-      if (child < vtx)
-         curr_ans1++;
-      else if (child > vtx)
-         curr_ans2++;
+      dfs(child, vtx, target, curr_sum, curr_cnt);
    }
 }
 int main()
 {
-   int n;
-   cin >> n;
-   for (int i = 0; i < n - 1; i++)
-   {
-      int x, y;
-      cin >> x >> y;
-      g[x].push_back(y);
-      g[y].push_back(x);
-   }
+   int n, q;
+   cin >> n >> q;
 
    for (int i = 1; i <= n; i++)
       cin >> val[i];
 
-   int ans = 0;
-   for (int i = 1; i <= n; i++)
+   for (int i = 0; i < n - 1; i++)
    {
-
-      if (val[i] == 1)
-      {
-         curr_ans1 = 1;
-         curr_ans2 = 1;
-         dfs1(i, 0);
-      }
-
-      ans = max(ans, max(curr_ans2, curr_ans1));
+      int v1, v2;
+      cin >> v1 >> v2;
+      g[v1].push_back(v2);
+      g[v2].push_back(v1);
    }
-   cout << ans;
+
+   while (q--)
+   {
+      int x;
+      long long k;
+      cin >> x >> k;
+
+      cnt = INT_MAX;
+      int curr_cnt = 1;
+      long long curr_sum = val[x];
+      dfs(x, 0, k, curr_sum, curr_cnt);
+      if (cnt == INT_MAX)
+         cout << -1 << endl;
+      else
+         cout << cnt << endl;
+   }
+
    return 0;
 }
