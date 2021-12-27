edcaa2c2d9d681a91c2f5b0f20c7ff8a8f868596
Divyansh Mittal <mittaldivyansh2212@gmail.com>
2021-12-27 19:59:07 +0530

    27/12/21

--- test.cpp
+++ test.cpp
@@ -1,66 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 int main()
 {
     int t;
     cin >> t;
+    
     while(t--)
     {
-        int n, c01, c10;
-        cin >> n >> c01 >> c10;
-        int arr[n];
-
-        for (int i = 0; i < n; ++i)
-            cin >> arr[i];
-        
-        int cost = 0;
-        if(arr[0] == arr[1])
+        int n;
+        cin >> n;
+        if(n % 4 == 0)
         {
-            if(arr[0] == 0)
-                cost += c01;
-            else
-                cost += c10;
-            arr[0] = !arr[1];
+            cout << "1 " << n << " " << endl;
+            continue;
         }
-        
-        for (int i = 1; i < n; i = i+2)
+        if(n % 4 == 1)
         {
-            if(arr[i] == arr[i-1])
-            {
-                if(i + 1 < n && arr[i] == arr[i+1])
-                {
-                    if(arr[i] == 0)
-                        cost += c01;
-                    if(arr[i] == 1)
-                        cost += c10;
-                    arr[i] = !arr[i];
-                }
-                else
-                {
-                    if(arr[i] == 1)
-                        cost += c10;
-                    if(arr[i] == 0)
-                        cost += c01;
-                    arr[i] = !arr[i];
-                }
-            }
-            else if(i+1 < n && arr[i] == arr[i+1])
-            {
-                
-                    if(arr[i+1] == 0)
-                        cost += c01;
-                    if(arr[i+1] == 1)
-                        cost += c10;
-                    arr[i+1] = !arr[i+1];
-            }
+            cout << "1 1 " << endl;
+            continue;
         }
-        for (int i = 0; i < n; ++i)
+        if(n % 4 == 2)
         {
-            cout << arr[i] << "  ";
+            cout << "2 " << n << " 1 " << endl;
+            continue;
         }
-        cout << endl;
-        cout << cost << endl;
+        if(n % 4 == 3)
+            cout << 0 << endl;
     }
+
     return 0;
 }
