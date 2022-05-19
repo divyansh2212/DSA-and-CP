5e2872ddbde035aedaa00e6a1a7f5419a312788b
Divyansh Mittal <mittaldivyansh2212@gmail.com>
2022-05-19 22:36:17 +0530

    19/05/22

---
+++ take u forward/Graph Series/vid10.cpp
@@ -0,0 +1,50 @@
+// Jai Shree Babosa!
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+class Solution
+{
+public:
+    void bfs(vector<vector<int>> &graph, vector<int> &color, int vtx, bool &flag)
+    {
+        queue<int> q;
+        q.push(vtx);
+        color[vtx] = 0;
+
+        while (!q.empty())
+        {
+            int top = q.front();
+            q.pop();
+
+            for (auto &child : graph[top])
+            {
+                if (color[child] != -1 && color[child] == color[top])
+                {
+                    flag = false;
+                    return;
+                }
+                else if (color[child] == -1)
+                {
+                    q.push(child);
+                    color[child] = !color[top];
+                }
+            }
+        }
+    }
+
+    bool isBipartite(vector<vector<int>> &graph)
+    {
+        int n = graph.size();
+        vector<int> color(n, -1);
+
+        bool flag = true;
+
+        for (int i = 0; i < n; i++)
+            if (color[i] == -1)
+                bfs(graph, color, i, flag);
+
+        return flag;
+    }
+};
